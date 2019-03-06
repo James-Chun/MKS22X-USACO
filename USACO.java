cb5ae350510ad8f2302f783966f9dcb5a34c5df4
James Chun <jchun00@stuy.edu>
2019-03-06 10:16:12 -0500

    started work

---
+++ USACO.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class USACO{
+  private int[][] pasture;
+  private int R;
+  private int C
+
+  public static int bronze(String filename){
+    File imput = new File(filename);
+    Scanner directions = new Scanner(imput);
+
+
+  }
+
+  public static int silver(String filename){
+
+  }
+}
