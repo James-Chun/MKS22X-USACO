16abb2bddc5e285e5b1cdf926e10e1b1d6752884
James Chun <jchun00@stuy.edu>
2019-03-07 09:51:29 -0500

    read file and made pasture

--- USACO.java
+++ USACO.java
@@ -1,18 +1,34 @@
 import java.util.*;
+import java.io.*;
 
 public class USACO{
   private int[][] pasture;
   private int R;
-  private int C
+  private int C;
+  private int E;
+  private int N;
 
-  public static int bronze(String filename){
+  public static int bronze(String filename) throws FileNotFoundException{
     File imput = new File(filename);
     Scanner directions = new Scanner(imput);
 
-
+    String temp = directions.nextLine();
+    int [][] pasture = new int[Integer.parseInt(temp.substring(0,1))][Integer.parseInt(temp.substring(2,3))];
+    return pasture[0].length;
   }
 
   public static int silver(String filename){
-
+    return 1;
   }
+
+
+  public static void main(String[] args){
+    try{
+      System.out.println(bronze(args[0]));
+    }catch(FileNotFoundException e){
+      System.out.println("File Not Found");
+    }
+  }
+
+
 }
