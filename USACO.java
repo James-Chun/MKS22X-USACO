a393e79cb0d562ac687976b1590b7c7d964f241c
James-Chun <jchun00@stuy.edu>
2019-03-10 03:13:19 -0400

    organized code and added comments

--- USACO.java
+++ USACO.java
@@ -3,63 +3,68 @@ import java.io.*;
 
 public class USACO{
   private static int[][] pasture;
-  private int R;
-  private int C;
-  private int E;
-  private int N;
+
 
   public static int bronze(String filename) throws FileNotFoundException{
-    File imput = new File(filename);
-    Scanner directions = new Scanner(imput);
+      File imput = new File(filename);  //scanning file
+      Scanner directions = new Scanner(imput);
 
-    //String temp = directions.nextLine();
-    int R = directions.nextInt();    //setting variables with values from first line
-    int C = directions.nextInt();
-    int E = directions.nextInt();
-    int N = directions.nextInt();
+      //------------------------------------------------------------------------------------------------------
 
-    int[][] pasture = new int[R][C]; //making pasture from values in first line
+      int R = directions.nextInt();    //setting variables with values from first line
+      int C = directions.nextInt();    //R is amount of rows, C is amount of columns
+      int E = directions.nextInt();    //E is final elevation
+      int N = directions.nextInt();    //N is number of commands
 
-    for (int r=0;r<pasture.length;r++){
-      for (int c=0;c<pasture[r].length;c++){  //adding values to pasture from the big block of numbers
-        pasture[r][c]=directions.nextInt();
-      }
-    }
+      int[][] pasture = new int[R][C]; //making pasture from values in first line
 
-
-    while (N>0){  //going through all directions using N (number of directions)
-      int row = directions.nextInt()-1;
-      int col = directions.nextInt()-1;
-      int largest = pasture[row][col];
-      int d = directions.nextInt();
-      for (int r=0;r<3;r++){
-        for (int c=0;c<3;c++){
-          if (pasture[r+row][c+col]>largest )largest = pasture[r+row][c+col];
+      for (int r=0;r<pasture.length;r++){
+        for (int c=0;c<pasture[r].length;c++){  //adding values to pasture from the big block of numbers
+          pasture[r][c]=directions.nextInt();
         }
       }
-      for (int r=0;r<3;r++){
-        for (int c=0;c<3;c++){
-          if (pasture[r+row][c+col]>largest-d) pasture[r+row][c+col] = largest - d;
-        }
-      }
-      N--;
 
-    }
+      //------------------------------------------------------------------------------------------------------
 
-    for (int r=0;r<pasture.length;r++){
-      for (int c=0;c<pasture[r].length;c++){
-        if (pasture[r][c] - E < 0 )pasture[r][c] = -1 * (pasture[r][c]-E);
-        else pasture[r][c]=0;
+      while (N>0){  //going through all directions using N
+          int row = directions.nextInt()-1;   //getting the row and columns from each set of commands
+          int col = directions.nextInt()-1;
+          int largest = pasture[row][col];
+          int d = directions.nextInt();
+          for (int r=0;r<3;r++){
+            for (int c=0;c<3;c++){
+              if (pasture[r+row][c+col]>largest )largest = pasture[r+row][c+col];
+            }                                                                       //find largest and subtract depth from it and if the numbers in that area are
+          }                                                                         //bigger than largest-depth then the cows will be able to catch up and start digging together
+          for (int r=0;r<3;r++){
+            for (int c=0;c<3;c++){
+              if (pasture[r+row][c+col]>largest-d) pasture[r+row][c+col] = largest - d;
+            }
+          }
+          N--;
       }
-    }
 
-    int sum = 0;
-    for (int r=0;r<pasture.length;r++){
-      for (int c=0;c<pasture[r].length;c++){
-        sum+=pasture[r][c];
+      //------------------------------------------------------------------------------------------------------
+
+      for (int r=0;r<pasture.length;r++){   //setting the final elevation
+          for (int c=0;c<pasture[r].length;c++){
+              if (pasture[r][c] - E < 0 )pasture[r][c] = -1 * (pasture[r][c]-E);
+              else pasture[r][c]=0;
+          }
       }
-    }
-    return sum*72*72;
+
+      //------------------------------------------------------------------------------------------------------
+
+      int sum = 0;    //calculating the total depth of lake
+      for (int r=0;r<pasture.length;r++){
+          for (int c=0;c<pasture[r].length;c++){
+              sum+=pasture[r][c];
+          }
+      }
+
+      //------------------------------------------------------------------------------------------------------
+
+      return sum*72*72;
   }
 
   private static void dig(int row, int col, int depth){
